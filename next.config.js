048e1f0c98b6fc2a0ab9080adb060cc6bd7733c7
obinna akujobi <phoenixz1@yahoo.com>
2026-03-15 15:53:02 +0100

    added more items to the stock

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "lpjmqxplwubvghpdlsur.supabase.co",
+        hostname: "*.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
     ],
